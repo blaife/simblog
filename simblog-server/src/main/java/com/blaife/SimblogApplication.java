d534621a3d6eb16d8bc9a7377fa876e52da51ece
BigBlackKnife <2767026270@qq.com>
2020-06-14 22:46:49 +0800

    server项目初始化

---
+++ simblog-server/src/main/java/com/blaife/SimblogApplication.java
@@ -0,0 +1,14 @@
+package com.blaife;
+
+import com.blaife.entity.User;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimblogApplication.class, args);
+    }
+
+}
---
+++ simblog-server/src/test/java/com/blaife/SimblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blaife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
