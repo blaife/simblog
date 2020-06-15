2cf4e5eab54cf787984c8853ff671708a47a01a4
BigBlackKnife <2767026270@qq.com>
2020-06-15 23:39:30 +0800

    Shiro配置文件

---
+++ simblog-server/src/main/java/com/blaife/config/ShiroConfig.java
@@ -0,0 +1,91 @@
+package com.blaife.config;
+
+import com.blaife.shiro.AccountRealm;
+import com.blaife.shiro.JwtFilter;
+import net.sf.saxon.ma.arrays.ArrayFunctionSet;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.mgt.SessionsSecurityManager;
+import org.apache.shiro.session.mgt.SessionManager;
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.spring.web.config.DefaultShiroFilterChainDefinition;
+import org.apache.shiro.spring.web.config.ShiroFilterChainDefinition;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.apache.shiro.web.session.mgt.DefaultWebSessionManager;
+import org.crazycake.shiro.RedisCacheManager;
+import org.crazycake.shiro.RedisSessionDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.Filter;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Blaife
+ * @description shiro配置文件
+ * @data 2020/6/15 22:13
+ */
+@Configuration
+public class ShiroConfig {
+
+    @Autowired
+    RedisSessionDAO redisSessionDAO;
+
+    @Autowired
+    RedisCacheManager redisCacheManager;
+
+    @Autowired
+    JwtFilter jwtFilter;
+
+    @Bean
+    public SessionManager sessionManager() {
+        DefaultWebSessionManager sessionManager = new DefaultWebSessionManager();
+
+        // inject redisSessionDAO
+        sessionManager.setSessionDAO(redisSessionDAO);
+        return sessionManager;
+    }
+
+    @Bean
+    public SessionsSecurityManager securityManager(AccountRealm accountRealm, SessionManager sessionManager) {
+        DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager(accountRealm);
+
+        //inject sessionManager
+        securityManager.setSessionManager(sessionManager);
+
+        // inject redisCacheManager
+        securityManager.setCacheManager(redisCacheManager);
+        return securityManager;
+    }
+
+    @Bean
+    public ShiroFilterChainDefinition shiroFilterChainDefinition() {
+        DefaultShiroFilterChainDefinition chainDefinition = new DefaultShiroFilterChainDefinition();
+
+        Map<String, String> filterMap = new LinkedHashMap<>();
+        filterMap.put("/**", "authc");
+
+        chainDefinition.addPathDefinitions(filterMap);
+        return chainDefinition;
+    }
+
+    @Bean("shiroFilterFactoryBean")
+    public ShiroFilterFactoryBean shiroFilterFactoryBean(SecurityManager securityManager,
+                                                         ShiroFilterChainDefinition shiroFilterChainDefinition) {
+        ShiroFilterFactoryBean shiroFilter = new ShiroFilterFactoryBean();
+        shiroFilter.setSecurityManager(securityManager);
+
+        Map<String, Filter> filters = new HashMap<>();
+        filters.put("jwt", jwtFilter);
+        shiroFilter.setFilters(filters);
+
+        Map<String, String> filterMap = shiroFilterChainDefinition.getFilterChainMap();
+
+        shiroFilter.setFilterChainDefinitionMap(filterMap);
+        return shiroFilter;
+    }
+
+}
