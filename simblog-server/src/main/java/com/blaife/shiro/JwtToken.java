70931e26e98e2fc6ec2277f47cd861669214350b
BigBlackKnife <2767026270@qq.com>
2020-06-15 23:41:01 +0800

    JWT中的Filter和Token提交

---
+++ simblog-server/src/main/java/com/blaife/shiro/JwtToken.java
@@ -0,0 +1,27 @@
+package com.blaife.shiro;
+
+import org.apache.shiro.authc.AuthenticationToken;
+
+/**
+ * @author Blaife
+ * @description token
+ * @data 2020/6/15 22:50
+ */
+public class JwtToken implements AuthenticationToken {
+
+    private String token;
+
+    public JwtToken(String token) {
+        this.token = token;
+    }
+
+    @Override
+    public Object getPrincipal() {
+        return token;
+    }
+
+    @Override
+    public Object getCredentials() {
+        return token;
+    }
+}
