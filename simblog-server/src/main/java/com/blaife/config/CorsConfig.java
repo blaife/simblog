41137b6ef81e3712320b41edbb5d55caf654346d
BigBlackKnife <2767026270@qq.com>
2020-06-21 19:43:38 +0800

    跨域问题处理

---
+++ simblog-server/src/main/java/com/blaife/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.blaife.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author Blaife
+ * @description 解决跨域问题
+ * @data 2020/6/21 19:38
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true)
+                .maxAge(3600)
+                .allowedHeaders("*");
+    }
+}
