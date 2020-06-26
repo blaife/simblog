bb3db51d907f2ae3c36d8646e72bbc6720af0db0
BigBlackKnife <2767026270@qq.com>
2020-06-26 21:55:52 +0800

    VUE前端页面生成

---
+++ simblog-vue/src/store/index.js
@@ -0,0 +1,16 @@
+/*数据管理*/
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
